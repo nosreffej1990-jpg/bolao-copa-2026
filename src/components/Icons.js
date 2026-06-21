0a1185a5031e7c45489362631315c1f95ebe85f7
nosreffej1990-jpg <nosreffej1990@gmail.com>
2026-06-21 14:03:58 -0300

    Fix: add missing RefreshCw icon causing client-side crash in Dashboard

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 export const Icons = {
+  RefreshCw: (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+      <polyline points="23 4 23 10 17 10"></polyline>
+      <polyline points="1 20 1 14 7 14"></polyline>
+      <path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path>
+    </svg>
+  ),
+
   Trophy: (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       <path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6" />
