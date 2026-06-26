c017812101ada217d7f97085f3ef7826a5b54d1a
nosreffej1990-jpg <nosreffej1990@gmail.com>
2026-06-25 21:28:51 -0300

    hollyshiiiiiit

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -159,5 +159,10 @@ export const Icons = {
       <polyline points="9 11 12 14 22 4" />
       <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11" />
     </svg>
+  ),
+  Bracket: (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+      <path d="M16 3H7a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h9" />
+    </svg>
   )
 };
